d51d894f45f15e39d27760c1ca56d36f9e034a71
CastroRodrigo <rodrigo.castro@correo.ucu.edu.uy>
2015-06-08 23:30:21 -0300

    Creando clases y constructores

---
+++ src/com/company/MagicPacket.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Rodrigo on 08/06/2015.
+ */
+public class MagicPacket {
+}
---
+++ src/com/company/WakeOnLan.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.io.*;
+import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class WakeOnLan {
+
+    private int puerto = 9;
+    private String macDestino;
+    private String ipBroadcast;
+
+    public String getMacDestino(){
+        return macDestino;
+    }
+
+    public void setMacDestino(String macDestino){
+        macDestino = this.macDestino;
+    }
+
+    public int getPuerto(){
+        return puerto;
+    }
+
+    public String getIpBroadcast(){
+        return ipBroadcast;
+    }
+
+    public void setIpBroadcast(String ipBroadcast){
+        ipBroadcast = this.ipBroadcast;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
