c6dae368070adc9b405a0c100f8ee4eb76967d27
CastroRodrigo <rodrigo.castro@correo.ucu.edu.uy>
2015-06-09 22:13:26 -0300

    Version que creo mejor: 1) Todo en una case 2) Metodo de analisis y transformacion de la MAC address terminado

--- src/com/company/WakeOnLan.java
+++ src/com/company/WakeOnLan.java
@@ -31,12 +31,23 @@ public class WakeOnLan {
     }
 
 
-    public byte crear (String macAddress){
+    public byte[] macToBytes (String macAddress) throws IllegalArgumentException {
 
-        byte[] mac = new byte [6];
-
-
+        byte[] macInBytes = new byte[6];
+        String[] macText = macAddress.split("-\\:"); // Corta los octetos de la MAC que esta en tipo String de a uno .
+        if (macText.length != 6) {
+            throw new IllegalArgumentException("Error, forma y tamaño de la Mac Address"); //verifica que la MAC tenga la forma/tamaño correcto.
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                macInBytes[i] = (byte) Integer.parseInt(macText[i], 16); // se parsea cada octeto de la MAC
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Error, algun digito de la Mac Address ingresada no es exadesimal");
+        }
+        return macInBytes;
     }
+
 
     public static void main(String[] args) {
 
